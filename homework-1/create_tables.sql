adbb46b6386da94af6e7c87a14fa4241cc640de8
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-07-11 22:44:19 +0400

    SQL command for table

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,31 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(10) UNIQUE,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id serial PRIMARY KEY,
+	customer_id varchar(10) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
+
+SELECT * FROM employees;
+
+SELECT * FROM customers;
+
+SELECT * FROM orders;
